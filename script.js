9c29473949fa4f83984ffe9fd211f70a5ed0a183
Momota Khatun <momotarajbd04@gmail.com>
2023-04-02 22:17:37 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*const name = document.querySelector(".name");
+function changecolor() {
+  name.style.color = "blue";
+}*/
+
+// 2nd practices
+const name = document.querySelector(".name");
+
+function changeColor(color) {
+  name.style.color = color;
+}
+
+// 3rd practices
+// const name = document.querySelector(".name");
+// const btn = document.querySelector("button");
+
+/*btn.addEventListener("click", function () {
+  name.style.color = "blue";
+});*/
+
+// btn.addEventListener("click", changeColor);
+// function changeColor() {
+//   name.style.color = "blue";
+// }
